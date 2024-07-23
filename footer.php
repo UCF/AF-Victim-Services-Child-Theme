1a2ea9d52b4e047b0b23377d528ba752e4b8a027
jgiron <joseph.giron@ucf.edu>
2024-07-23 16:45:26 -0400

    add footer file from UCF WordPress Theme and added Izzy script

---
+++ footer.php
@@ -0,0 +1,14 @@
+			</div>
+		</main>
+		<?php echo ucfwp_get_footer_markup(); ?>
+		<?php wp_footer(); ?>
+		<script type="text/javascript">
+		var RippleLiveChat_SiteId = 3000078;
+		(function() {
+		var s = document.createElement('script'); s.type = 'text/javascript';
+		s.src = 'https://cdn.ripplegroup.ca/ripple.livechatclient/js/rlv-loader.min.js?' + (new Date().getTime());
+		document.body.appendChild(s);
+		})();
+		</script>
+	</body>
+</html>
